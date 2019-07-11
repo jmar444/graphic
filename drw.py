6c599cdb50ce970a8a1b15c15b6a6ec9479f45ae
Jmar444 <juliusmar4@yandex.ru>
2019-07-11 22:48:05 +0300

    edit

--- drw.py
+++ drw.py
@@ -5,9 +5,9 @@ DEFAULT_WIDTH = 1000
 DEFAULT_HEIGHT = 1000
 
 def emptydict(x=DEFAULT_WIDTH, y=DEFAULT_HEIGHT):
-	return dict(zip(list(r.C2(i, j) for i in range(-x//2, x//2+1) for j in range(-y//2, y//2+1)), [[  0,  0,  0] for k in range(x*y)]))
+	return dict(zip(list(r.C2(i, j) for i in range(-x//2, x//2+1) for j in range(-y//2, y//2+1)), [(  0,  0,  0) for k in range(x*y)]))
 def fulldict(x=DEFAULT_WIDTH, y=DEFAULT_HEIGHT):
-	return dict(zip(list(r.C2(i, j) for i in range(-x//2, x//2+1) for j in range(-y//2, y//2+1)), [[255,255,255] for k in range(x*y)]))
+	return dict(zip(list(r.C2(i, j) for i in range(-x//2, x//2+1) for j in range(-y//2, y//2+1)), [(255,255,255) for k in range(x*y)]))
 
 class Layer():
 	def __init__(self, x=DEFAULT_WIDTH, y=DEFAULT_HEIGHT, d=0, center=r.C2(-DEFAULT_WIDTH//2, -DEFAULT_HEIGHT//2)):
@@ -39,6 +39,9 @@ class Layer():
 								  min(self_arr[2], other_arr[2]) ]})
 		return Layer(self.width, self.height, result)
 
+	def edit(self, coordinate, color):
+		self.dict[coordinate] = color	
+
 	def manipulate(self, f):
 		result = emptydict(self.width, self.height)
 		keys = self.dict.keys()
@@ -52,7 +55,7 @@ class Layer():
 	def drawlayer(self, image):
 		keys = self.dict.keys()
 		for key in keys:
-			color = tuple(self.dict[key])
+			color = self.dict[key]
 			key.draw(image, color)
 
 
--- main.py
+++ main.py
@@ -6,7 +6,10 @@ from PIL import Image
 def rotate90(x):
 	return r.C2(0,1)*x
 
-a = drw.Layer(1000, 1000, drw.emptydict(1000, 1000))
+a = drw.Layer(999, 999, drw.emptydict(1000, 1000))
+for i in range(-500, 501):
+	a.edit(r.C2(i), (255,255,255))
+
 
 image = Image.new("RGB", (1000, 1000), "white")
 
